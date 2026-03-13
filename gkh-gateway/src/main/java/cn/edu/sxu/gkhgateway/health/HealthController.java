a30baab9c0a39cac9eb28b05880e0e7153731a54
liujiarong <liujiarong@zjcnhy.com.cn>
2026-03-13 17:31:15 +0800

    3/13

--- gkh-gateway/src/main/java/cn/edu/sxu/gkhgateway/health/HealthController.java
+++ gkh-gateway/src/main/java/cn/edu/sxu/gkhgateway/health/HealthController.java
@@ -1,4 +1,19 @@
 package cn.edu.sxu.gkhgateway.health;
 
+
+import cn.edu.sxu.common.BaseResponse;
+import cn.edu.sxu.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
 public class HealthController {
+
+    @GetMapping
+    public BaseResponse<String> health(){
+        return ResultUtils.success("health");
+    }
+
 }
